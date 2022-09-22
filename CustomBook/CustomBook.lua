965e6c7df7bc1a6774a78882583d09d2838e3e11
Rickoff <38435435+rickoff@users.noreply.github.com>
2022-09-22 15:04:08 +0200

    Update CustomBook.lua

--- CustomBook/CustomBook.lua
+++ CustomBook/CustomBook.lua
@@ -1,7 +1,7 @@
 --[[
 CustomBook
-tes3mp 0.8.0
-Version 0.4
+tes3mp 0.8.1
+Version 0.5
 Rewrite by Rickoff original script by Jakob
 ---------------------------
 DESCRIPTION :
@@ -23,15 +23,15 @@ CustomBook = require("custom.CustomBook")
 ---------------------------
 ]]
 
-local config = {}
-config.MainGUI = 999363
-config.EditTitleGUI = 999364
-config.EditTextGUI = 99965
-config.ClearTextGUI = 999366
-config.StyleGUI = 999367
+local cfg = {}
+cfg.MainGUI = 999363
+cfg.EditTitleGUI = 999364
+cfg.EditTextGUI = 99965
+cfg.ClearTextGUI = 999366
+cfg.StyleGUI = 999367
 
 local trad = {}
-trad.mainMenu = color.Orange .. "BIENVENUE DANS LA BIBLIOTHEQUE.\n" .. color.Yellow .. "\nTitre : " .. color.White ..  "pour créer un livre en lui donnant un titre.\n\n"  .. color.Yellow .. "Texte : " .. color.White ..  "pour ajouter une ligne à votre livre en cours.\n\n" .. color.Yellow .. "Nouveau Texte : " .. color.White .. "pour effacer et réecrire toute les lignes.\n" .. color.Default
+trad.mainMenu = color.Orange .. "MENU IMPRIMERIE\n" .. color.Yellow .. "\nTitre : " .. color.White ..  "pour créer un livre en lui donnant un titre.\n\n"  .. color.Yellow .. "Texte : " .. color.White ..  "pour ajouter une ligne à votre livre en cours.\n\n" .. color.Yellow .. "Nouveau Texte : " .. color.White .. "pour effacer et réecrire toute les lignes.\n" .. color.Default
 trad.optionMenu = "Titre;Texte;Nouveau Texte;Liste Style;Selection Style;Effacer;Imprimer;Retour;Fermer"
 trad.editTitle = "Entrer un titre pour votre livre"
 trad.editText = "Entrer du texte pour ajouter une ligne à votre livre."
@@ -49,73 +49,75 @@ trad.wrote = "Vous avez écrit un livre!"
 trad.needPaper = "Il vous manque le papier pour écrire un livre."
 trad.copyBook = "Vous avez une copie de votre livre!"
 
-local CustomBook = {}
-
-CustomBook.currentBooks = {} --used to store players individual in progress books
-CustomBook.bookStyles = {}
-table.insert(CustomBook.bookStyles, {model = "m\\Text_Octavo_03.nif", icon = "m\\tx_octavo_03.tga", scroll = false, name = "Red Book"} )
-table.insert(CustomBook.bookStyles, {model = "m\\Text_Parchment_02.nif", icon = "m\\Tx_parchment_02.tga", scroll = true, name = "Letter"} )
-table.insert(CustomBook.bookStyles, {model = "m\\Text_Note_02.nif", icon = "m\\Tx_note_02.tga", scroll = true, name = "Note"} )
-table.insert(CustomBook.bookStyles, {model = "m\\Text_Octavo_06.nif", icon = "m\\Tx_book_03.tga", scroll = false, name = "Lesson of Vivec"} )
-
-CustomBook.nameSymbol = "~" --the symbol used before and after book names to differintiate them from vanila books
+local EditBook = {
+	nameSymbol = "~",
+	currentBooks = {},
+	bookStyles = {
+		{model = "m\\Text_Octavo_03.nif", icon = "m\\tx_octavo_03.tga", scroll = false, name = "Livre Rouge"},
+		{model = "m\\Text_Parchment_02.nif", icon = "m\\Tx_parchment_02.tga", scroll = true, name = "Lettre"},
+		{model = "m\\Text_Note_02.nif", icon = "m\\Tx_note_02.tga", scroll = true, name = "Note"},
+		{model = "m\\Text_Octavo_06.nif", icon = "m\\Tx_book_03.tga", scroll = false, name = "Parchemin de Vivec"}	
+	},
+}
 
 local msg = function(pid, text)
-	tes3mp.SendMessage(pid, color.GreenYellow .. "[BookWriting] " .. color.Default .. text .. "\n" .. color.Default)
+	tes3mp.SendMessage(pid, color.GreenYellow .. "[Edition] " .. color.Default .. text .. "\n" .. color.Default)
 end
 
-function CustomBook.onCommand(pid, cmd)
+local CustomBook = {}
+
+CustomBook.onCommand = function(pid, cmd)
     local name = Players[pid].name:lower()
     if cmd[2] == "clear" then
-        CustomBook.currentBooks[name] = nil
+        EditBook.currentBooks[name] = nil
         msg(pid, trad.clear)
     elseif cmd[2] == "title" then
         CustomBook.startBook(name)
-        CustomBook.currentBooks[name].title = table.concat(cmd, " ", 3)
+        EditBook.currentBooks[name].title = table.concat(cmd, " ", 3)
         msg(pid, trad.title)
     elseif cmd[2] == "addtext" then
         CustomBook.startBook(name)
 		message = (table.concat(cmd, " ", 3) .. "<p>")
-        CustomBook.currentBooks[name].text = CustomBook.currentBooks[name].text .. message
+        EditBook.currentBooks[name].text = EditBook.currentBooks[name].text .. message
         msg(pid, trad.addText)
     elseif cmd[2] == "settext" then
         CustomBook.startBook(name)
 		message = (table.concat(cmd, " ", 3) .. "<p>")
-        CustomBook.currentBooks[name].text = message
+        EditBook.currentBooks[name].text = message
         msg(pid, trad.setText)
     elseif cmd[2] == "done" then
-        if CustomBook.currentBooks[name] == nil then
+        if EditBook.currentBooks[name] == nil then
             msg(pid, trad.noName)
         else
             CustomBook.createBook(pid)
         end
     elseif cmd[2] == "liststyle" or cmd[2] == "liststyles" then
         msg(pid, trad.optionStyle)
-        for i, bookType in pairs(CustomBook.bookStyles) do
+        for i, bookType in pairs(EditBook.bookStyles) do
             msg(pid, tostring(i) .. ": " .. bookType.name)
         end
     elseif cmd[2] == "setstyle" then
         CustomBook.startBook(name)
         if tonumber(cmd[3]) == nil then return end
         if tonumber(cmd[3]) < 1 then return end
-        if tonumber(cmd[3]) > #CustomBook.bookStyles then return end
-        CustomBook.currentBooks[name].type = tonumber(cmd[3])
+        if tonumber(cmd[3]) > #EditBook.bookStyles then return end
+        EditBook.currentBooks[name].type = tonumber(cmd[3])
         msg(pid, trad.style)
     else
         msg(pid, trad.help)
     end
 end
 
-function CustomBook.startBook(name)
-    if CustomBook.currentBooks[name] == nil then
-        CustomBook.currentBooks[name] = {}
-        CustomBook.currentBooks[name].title = "Empty Title"
-        CustomBook.currentBooks[name].text = ""
-        CustomBook.currentBooks[name].type = 1
+CustomBook.startBook = function(name)
+    if EditBook.currentBooks[name] == nil then
+        EditBook.currentBooks[name] = {}
+        EditBook.currentBooks[name].title = "Empty Title"
+        EditBook.currentBooks[name].text = ""
+        EditBook.currentBooks[name].type = 1
     end
 end
 
-function CustomBook.createBook(pid)
+CustomBook.createBook = function(pid)
     local name = Players[pid].name:lower()
 	if inventoryHelper.containsItem(Players[pid].data.inventory,"sc_paper plain") then
 		inventoryHelper.removeItem(Players[pid].data.inventory,"sc_paper plain",1)
@@ -127,39 +129,38 @@ function CustomBook.createBook(pid)
         msg(pid, color.Red .. trad.needPaper)
 		return
 	end
-    local model = CustomBook.bookStyles[CustomBook.currentBooks[name].type].model
-    local icon = CustomBook.bookStyles[CustomBook.currentBooks[name].type].icon
-    local scroll = CustomBook.bookStyles[CustomBook.currentBooks[name].type].scroll
+    local model = EditBook.bookStyles[EditBook.currentBooks[name].type].model
+    local icon = EditBook.bookStyles[EditBook.currentBooks[name].type].icon
+    local scroll = EditBook.bookStyles[EditBook.currentBooks[name].type].scroll
     local book = {}
     book["weight"] = 1
     book["icon"] = icon
     book["skillId"] = "-1"
     book["model"] = model
-    book["text"] = CustomBook.currentBooks[name].text
+    book["text"] = EditBook.currentBooks[name].text
     book["value"] = 1
     book["scrollState"] = scroll
-    book["name"] = CustomBook.nameSymbol .. CustomBook.currentBooks[name].title .. CustomBook.nameSymbol
+    book["name"] = EditBook.nameSymbol .. EditBook.currentBooks[name].title .. EditBook.nameSymbol
+
     for id,n in pairs(RecordStores["book"].data.generatedRecords) do
         if n.name == book["name"] and n.text == book["text"] then
             msg(pid, trad.copyBook)
             inventoryHelper.addItem(Players[pid].data.inventory, id, 1)
-            Players[pid]:Save()
-            Players[pid]:LoadInventory()
-            Players[pid]:LoadEquipment()
-            Players[pid]:LoadQuickKeys()
+			Players[pid]:QuicksaveToDrive()
+			local itemRef = {refId = id, count = 1, charge = -1, soul = ""}
+			Players[pid]:LoadItemChanges({itemRef}, enumerations.inventory.ADD)
             return
         end
     end
     local bookId = CustomBook.nuCreateBookRecord(pid, book)
     Players[pid]:AddLinkToRecord("book", bookId)
     inventoryHelper.addItem(Players[pid].data.inventory, bookId, 1)
-    Players[pid]:Save()
-	Players[pid]:LoadInventory()
-    Players[pid]:LoadEquipment()
-    Players[pid]:LoadQuickKeys()
+    Players[pid]:QuicksaveToDrive()
+	local itemRef = {refId = bookId, count = 1, charge = -1, soul = ""}
+	Players[pid]:LoadItemChanges({itemRef}, enumerations.inventory.ADD)
 end
 
-function CustomBook.nuCreateBookRecord(pid, recordTable)
+CustomBook.nuCreateBookRecord = function(pid, recordTable)
     local recordStore = RecordStores["book"]
     local id = recordStore:GenerateRecordId()
     local savedTable = recordTable
@@ -176,95 +177,90 @@ function CustomBook.nuCreateBookRecord(pid, recordTable)
     tes3mp.SendRecordDynamic(pid, true, false)
     return id
 end
-
-function CustomBook.onMainGui(pid)
-    CustomBook.showMainGUI(pid)
-end
  
-function CustomBook.showMainGUI(pid)
-    tes3mp.CustomMessageBox(pid, config.MainGUI, trad.mainMenu, trad.optionMenu)
+CustomBook.showMainGUI = function(pid)
+    tes3mp.CustomMessageBox(pid, cfg.MainGUI, trad.mainMenu, trad.optionMenu)
 end
 
-function CustomBook.showEditTitlePrompt(pid)
-    return tes3mp.InputDialog(pid, config.EditTitleGUI, trad.editTitle, "")
+CustomBook.showEditTitlePrompt = function(pid)
+    return tes3mp.InputDialog(pid, cfg.EditTitleGUI, trad.editTitle, "")
 end
 
-function CustomBook.showEditTextPrompt(pid)
-    return tes3mp.InputDialog(pid, config.EditTextGUI, trad.editText, "")
+CustomBook.showEditTextPrompt = function(pid)
+    return tes3mp.InputDialog(pid, cfg.EditTextGUI, trad.editText, "")
 end
 
-function CustomBook.showClearTextPrompt(pid)
-    return tes3mp.InputDialog(pid, config.ClearTextGUI, trad.clearText, "")
+CustomBook.showClearTextPrompt = function(pid)
+    return tes3mp.InputDialog(pid, cfg.ClearTextGUI, trad.clearText, "")
 end
 
-function CustomBook.showStylePrompt(pid)
-    return tes3mp.InputDialog(pid, config.StyleGUI, trad.selectStyle, "")
+CustomBook.showStylePrompt = function(pid)
+    return tes3mp.InputDialog(pid, cfg.StyleGUI, trad.selectStyle, "")
 end
 
-function CustomBook.OnGUIAction(pid, idGui, data)   
-    if idGui == config.MainGUI then -- Main
-        if tonumber(data) == 0 then --Titre
+CustomBook.OnGUIAction = function(pid, idGui, data)
+   
+    if idGui == cfg.MainGUI then
+        if tonumber(data) == 0 then
             CustomBook.showEditTitlePrompt(pid)
             return true
-        elseif tonumber(data) == 1 then --Texte
+        elseif tonumber(data) == 1 then
             CustomBook.showEditTextPrompt(pid)
             return true
-        elseif tonumber(data) == 2 then --Nouveau Texte
+        elseif tonumber(data) == 2 then
             CustomBook.showClearTextPrompt(pid)
             return true
-        elseif tonumber(data) == 3 then --Liste Style
+        elseif tonumber(data) == 3 then
 			message = "/book liststyles"
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
-        elseif tonumber(data) == 4 then -- Selection Style
+            return CustomBook.showMainGUI(pid)
+        elseif tonumber(data) == 4 then
             CustomBook.showStylePrompt(pid)
             return true
-        elseif tonumber(data) == 5 then -- Effacer
+        elseif tonumber(data) == 5 then
 			message = "/book clear"
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
-        elseif tonumber(data) == 6 then -- Imprimer
+            return CustomBook.showMainGUI(pid)
+        elseif tonumber(data) == 6 then
 			message = "/book done"
             eventHandler.OnPlayerSendMessage(pid, message)
             return true
-        elseif tonumber(data) == 7 then -- retour
-			Players[pid].currentCustomMenu = "menu player"
-			menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)
+        elseif tonumber(data) == 7 then
             return true
-        elseif tonumber(data) == 8 then -- fermer
+        elseif tonumber(data) == 8 then
             return true			
         end
-    elseif idGui == config.EditTitleGUI then
-        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then --Close/Nothing Selected
+    elseif idGui == cfg.EditTitleGUI then
+        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then
             return true
         else
 			message = ("/book title " .. data)
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
+            return CustomBook.showMainGUI(pid)
         end       
-    elseif idGui == config.EditTextGUI then
-        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then --Close/Nothing Selected
+    elseif idGui == cfg.EditTextGUI then
+        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then
             return true
         else
 			message = ("/book addtext " .. data)
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
+            return CustomBook.showMainGUI(pid)
         end       
-    elseif idGui == config.ClearTextGUI then
-        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then --Close/Nothing Selected
+    elseif idGui == cfg.ClearTextGUI then
+        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then
             return true
         else
 			message = ("/book settext " .. data)
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
+            return CustomBook.showMainGUI(pid)
         end
-    elseif idGui == config.StyleGUI then
-        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then --Close/Nothing Selected
+    elseif idGui == cfg.StyleGUI then
+        if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then
             return true
         else
 			message = ("/book setstyle " .. tonumber(data))
             eventHandler.OnPlayerSendMessage(pid, message)
-            return CustomBook.onMainGui(pid)
+            return CustomBook.showMainGUI(pid)
         end 	
 	end
 end
@@ -274,3 +270,5 @@ customCommandHooks.registerCommand("booking", CustomBook.showMainGUI)
 customEventHooks.registerHandler("OnGUIAction", function(eventStatus, pid, idGui, data)
 	if CustomBook.OnGUIAction(pid, idGui, data) then return end
 end)
+
+return CustomBook
