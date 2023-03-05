0f2705e8c5dc2c5f69ba93787d76b1fb38aa366e
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-03-05 22:46:19 +0100

    Update PreventMerchantEquipFix.lua

--- PreventEquipMerchant/PreventMerchantEquipFix.lua
+++ PreventEquipMerchant/PreventMerchantEquipFix.lua
@@ -21,34 +21,17 @@ local NpcBarterList = jsonInterface.load("custom/PreventMerchantEquipList.json")
 --------------
 -- FUNCTION --
 --------------
-local function LoadData()
-
-	NpcBarterList = jsonInterface.load("custom/PreventMerchantEquipList.json")	
-	
-end
-
 local function SaveData()
-
-	jsonInterface.save("custom/PreventMerchantEquipList.json", NpcBarterList)
-	
-	LoadData()
-	
+	jsonInterface.save("custom/PreventMerchantEquipList.json", NpcBarterList)	
 end
 
 local function GetIndexBarter(uniqueIndex)
-
-	for actorIndex, bool in pairs(NpcBarterList) do
-		
-		if uniqueIndex == actorIndex then
-		
-			return true
-			
-		end
-		
+	for actorIndex, bool in pairs(NpcBarterList) do		
+		if uniqueIndex == actorIndex then		
+			return true			
+		end		
 	end
-
-	return false
-	
+	return false	
 end
 
 -------------
@@ -56,81 +39,46 @@ end
 -------------
 local PreventMerchantEquipFix = {}
 
-PreventMerchantEquipFix.OnObjectDialogueChoice = function(eventStatus, pid, cellDescription, objects)
-
-	if Players[pid] and Players[pid]:IsLoggedIn() then
-	
-		local ObjectIndex
-		local ObjetDialogueType
-		
-		for _, object in pairs(objects) do
-			ObjectIndex = object.uniqueIndex
-			ObjetDialogueType = tableHelper.getIndexByValue(enumerations.dialogueChoice, object.dialogueChoiceType)
-		end	
-		
-		if ObjectIndex and ObjetDialogueType then
-			
-			if ObjetDialogueType == "BARTER" then
-			
-				if not GetIndexBarter(ObjectIndex) then
-					
-					NpcBarterList[ObjectIndex] = true
-					
-					SaveData()
-					
-				end
-				
+PreventMerchantEquipFix.OnObjectDialogueChoice = function(eventStatus, pid, cellDescription, objects)	
+	local ObjectIndex
+	local ObjetDialogueType
+	for _, object in pairs(objects) do
+		ObjectIndex = object.uniqueIndex
+		ObjetDialogueType = tableHelper.getIndexByValue(enumerations.dialogueChoice, object.dialogueChoiceType)
+	end	
+	if ObjectIndex and ObjetDialogueType then
+		if ObjetDialogueType == "BARTER" then
+			if not GetIndexBarter(ObjectIndex) then
+				NpcBarterList[ObjectIndex] = true
+				SaveData()
 			end
-			
 		end
 	end
 end
 
-PreventMerchantEquipFix.OnActorEquipment = function(eventStatus, pid, cellDescription, actors)
-
-	if Players[pid] and Players[pid]:IsLoggedIn() then
-		
-		for _, actor in pairs(actors) do
-		
-			local ActorIndex = actor.uniqueIndex
-	
-			if ActorIndex then
-				
-				if NpcBarterList[ActorIndex] then
-
-					LoadedCells[cellDescription]:LoadActorEquipment(pid, LoadedCells[cellDescription].data.objectData, {ActorIndex})
-				
-					return customEventHooks.makeEventStatus(false, false)	
-					
-				end
-				
-			end
-			
+PreventMerchantEquipFix.OnActorEquipment = function(eventStatus, pid, cellDescription, actors)		
+	for _, actor in pairs(actors) do
+		local ActorIndex = actor.uniqueIndex
+		if ActorIndex and NpcBarterList[ActorIndex] then
+			LoadedCells[cellDescription]:LoadActorEquipment(pid, LoadedCells[cellDescription].data.objectData, {ActorIndex})
+			return customEventHooks.makeEventStatus(false, false)
 		end
-		
-	end
-	
+	end	
 end
 
 PreventMerchantEquipFix.OnServerPostInit = function(eventStatus)
-
-	if NpcBarterList == nil then
-	
-		NpcBarterList = {}
-		
-		SaveData()
-		
-	end
-	
+	if NpcBarterList == nil then	
+		NpcBarterList = {}		
+		SaveData()		
+	end	
 end
 
 ------------
 -- EVENTS --
 ------------
-customEventHooks.registerHandler("OnObjectDialogueChoice", PreventMerchantEquipFix.OnObjectDialogueChoice)
-
-customEventHooks.registerHandler("OnServerPostInit", PreventMerchantEquipFix.OnServerPostInit)
-
 customEventHooks.registerValidator("OnActorEquipment", PreventMerchantEquipFix.OnActorEquipment)
+
+customEventHooks.registerHandler("OnObjectDialogueChoice", PreventMerchantEquipFix.OnObjectDialogueChoice)
+customEventHooks.registerHandler("OnServerPostInit", PreventMerchantEquipFix.OnServerPostInit)
 
 return PreventMerchantEquipFix
