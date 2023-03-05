fce8b94dd6a22d9e3bdfa84970656f7aead05949
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-03-05 22:42:52 +0100

    Update PreventDamage.lua

--- PreventDamage/PreventDamage.lua
+++ PreventDamage/PreventDamage.lua
@@ -1,6 +1,6 @@
 --[[
 PreventDamage
-tes3mp 0.8.0
+tes3mp 0.8.1
 ---------------------------
 INSTALLATION:
 Save the file as PreventDamage.lua inside your server/scripts/custom folder.
@@ -22,32 +22,28 @@ local DisablePvp = { player = {} }
 
 local PreventDamage = {}
 
-PreventDamage.Command = function(pid)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		local PlayerName = GetName(pid)
-		if DisablePvp.player[PlayerName] then
-			DisablePvp.player[PlayerName] = nil
-			tes3mp.MessageBox(pid, -1, color.Red.."Pvp mod enable !")		
-		else
-			DisablePvp.player[PlayerName] = true
-			tes3mp.MessageBox(pid, -1, color.Green.."Pvp mod disable !")			
-		end
+PreventDamage.Command = function(pid)	
+	local PlayerName = GetName(pid)
+	if DisablePvp.player[PlayerName] then
+		DisablePvp.player[PlayerName] = nil
+		tes3mp.MessageBox(pid, -1, color.Red.."Pvp mod enable !")		
+	else
+		DisablePvp.player[PlayerName] = true
+		tes3mp.MessageBox(pid, -1, color.Green.."Pvp mod disable !")			
 	end
 end
 
 PreventDamage.OnObjectHit = function(eventStatus, pid, cellDescription, objects, targetPlayers)
-	if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then	
-		for targetPid, targetPlayer in pairs(targetPlayers) do
-			if Players[targetPid] ~= nil and Players[targetPid]:IsLoggedIn() then			
-				local targetPlayerName = GetName(targetPid)
-				if DisablePvp.player[targetPlayerName] and targetPlayer.hittingPid and targetPlayer.hit.success then
-					local targetHealthCurrent = tes3mp.GetHealthCurrent(targetPid)
-					local Damage = targetPlayer.hit.damage
-					local newHealth = targetHealthCurrent + Damage
-					tes3mp.SetHealthCurrent(targetPid, newHealth)				
-					tes3mp.SendStatsDynamic(targetPid)
-					tes3mp.MessageBox(targetPlayer.hittingPid, -1, color.Red..targetPlayerName.." has pvp mode disable !")				
-				end
+	for targetPid, targetPlayer in pairs(targetPlayers) do
+		if Players[targetPid] and Players[targetPid]:IsLoggedIn() then			
+			local targetPlayerName = GetName(targetPid)
+			if DisablePvp.player[targetPlayerName] and targetPlayer.hittingPid and targetPlayer.hit.success then
+				local targetHealthCurrent = tes3mp.GetHealthCurrent(targetPid)
+				local Damage = targetPlayer.hit.damage
+				local newHealth = targetHealthCurrent + Damage
+				tes3mp.SetHealthCurrent(targetPid, newHealth)				
+				tes3mp.SendStatsDynamic(targetPid)
+				tes3mp.MessageBox(targetPlayer.hittingPid, -1, color.Red..targetPlayerName.." has pvp mode disable !")				
 			end
 		end
 	end
