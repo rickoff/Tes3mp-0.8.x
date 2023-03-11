a52e95208b0d69d93e519ac583bea94754dd5095
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-03-12 00:43:55 +0100

    Create MovePlease.lua

---
+++ MovePlease/MovePlease.lua
@@ -0,0 +1,47 @@
+--[[
+MovePlease by Rickoff
+tes3mp 0.8.1
+---------------------------
+DESCRIPTION :
+I was tired of being stuck in hallways.
+activating an actor with the hands to cast a spell makes him move, only in the interior cells.
+---------------------------
+INSTALLATION:
+Save the file as MovePlease.lua inside your server/scripts/custom folder.
+
+Edits to customScripts.lua
+MovePlease = require("custom.MovePlease")
+---------------------------
+]]
+
+local MovePlease = {}
+
+MovePlease.OnActivatedObject = function(eventStatus, pid, cellDescription, objects)
+
+	if not tes3mp.IsInExterior(pid) then
+	
+		local ObjectIndex
+		local ObjectRefid
+		
+		for _, object in pairs(objects) do
+			ObjectIndex = object.uniqueIndex
+			ObjectRefid = object.refId
+		end
+
+		if ObjectIndex and ObjectRefid then
+
+			if tableHelper.containsValue(LoadedCells[cellDescription].data.packets.actorList, ObjectIndex) and tes3mp.GetDrawState(pid) == 2 then
+		
+				logicHandler.RunConsoleCommandOnObject(pid, "PlayGroup, hit2, 1", cellDescription, ObjectIndex, false)	
+				logicHandler.RunConsoleCommandOnObject(pid, "AIWander, 512, 1, 0", cellDescription, ObjectIndex, false)
+				
+				return customEventHooks.makeEventStatus(false,false) 
+
+			end			
+		end
+	end
+end
+
+customEventHooks.registerValidator("OnObjectActivate", MovePlease.OnActivatedObject)
+
+return MovePlease
