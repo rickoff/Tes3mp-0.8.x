83a5c95b9626cb224d31e899b8f121071c8fab41
Rickoff <38435435+rickoff@users.noreply.github.com>
2023-10-07 13:59:50 +0200

    Update AnimationMenu.lua

--- AnimationScript/AnimationMenu.lua
+++ AnimationScript/AnimationMenu.lua
@@ -27,7 +27,7 @@ local PlayerAnimationList = {}
 -- CONFIG --
 ------------
 local cfg = {
-	OnServerPostInitHandler = true
+	OnServerPostInit = false
 }
 
 local guiID = {
@@ -53,9 +53,9 @@ end
 -------------
 local AnimationMenu = {}
 
-AnimationMenu.OnServerPostInitHandler = function()
+AnimationMenu.OnServerPostInit = function()
 
-	if cfg.OnServerPostInitHandler then
+	if cfg.OnServerPostInit then
 		local recordStore = RecordStores["spell"]
 		recordStore.data.permanentRecords["sittingAnim_paralyze"] = {
 			name = "Paralysie animation (/anim)",
@@ -74,65 +74,88 @@ AnimationMenu.OnServerPostInitHandler = function()
 			}}
 		}
 		recordStore:Save()
+		tes3mp.LogAppend(enumerations.log.ERROR, "AnimationMenu Terminate")
 	end
 end
 
 AnimationMenu.showAnimMenu = function(pid)
+
+	if tes3mp.GetHealthCurrent(pid) <= 0 then
+		Players[pid].currentCustomMenu = "resurrect"		
+		menuHelper.DisplayMenu(pid, Players[pid].currentCustomMenu)	
+		return
+	end
 	
 	local message = color.Orange.. traduction.title
-
+	
 	optionList = traduction.option
 
 	tes3mp.CustomMessageBox(pid, guiID.animMenu, message, optionList)
-	
+
 end
 
 AnimationMenu.OnGUIAction = function(eventStatus, pid, idGui, data)
-	
+
 	if idGui == guiID.animMenu then
-
+	
 		local PlayerName = GetName(pid)
-
+		
+		local PlayerRace = Players[pid].data.character.race
+		
+		local PlayerGender = Players[pid].data.character.gender
+		
 		local cellDescription = tes3mp.GetCell(pid)
-
+		
 		local Model = "base_anim.nif"
-
+		
+		if PlayerGender == 0 then
+		
+			Model = "base_anim_female.nif"	
+			
+		end
+		
+		if PlayerRace == "argonian" or PlayerRace == "khajiit" then
+		
+			Model = "base_animkna.nif"
+			
+		end
+		
 		local Animation = "idle"
-
+		
 		if tonumber(data) >= 0 and tonumber(data) < 9 then
-
+		
 			table.insert(Players[pid].data.spellbook, "sittingAnim_paralyze")
-
+			
 			Players[pid]:LoadSpellbook()
-
+			
 			Model = "va_sitting.nif"
 
 			logicHandler.RunConsoleCommandOnPlayer(pid, "PCForce3rdPerson", false)
 
 			logicHandler.RunConsoleCommandOnPlayer(pid, "DisablePlayerViewSwitch", false)	
-
+			
 		end
-
+		
 		if tonumber(data) == 0 then
 
 			Animation = "idle2"
-
+			
 		elseif tonumber(data) == 1 then
 
 			Animation = "idle9"
-
+			
 		elseif tonumber(data) == 2 then
 
 			Animation = "idle7"	
-
+		
 		elseif tonumber(data) == 3 then
 
 			Animation = "idle8"					
-
+			
 		elseif tonumber(data) == 4 then
 
 			Animation = "idle3"				
-
+	
 		elseif tonumber(data) == 5 then
 
 			Animation = "idle4"	
@@ -140,33 +163,33 @@ AnimationMenu.OnGUIAction = function(eventStatus, pid, idGui, data)
 		elseif tonumber(data) == 6 then
 
 			Animation = "idle5"		
-
+			
 		elseif tonumber(data) == 7 then
 
 			Animation = "idle6"			
 
 		elseif tonumber(data) == 8 then
-
+			
 			Model = "anim_dancinggirl.nif"
 
 			Animation = "idle9"	
-
+			
 		elseif tonumber(data) == 9 then
-
-			logicHandler.RunConsoleCommandOnPlayer(pid, "player->removespell sittingAnim_paralyze")
-
+		
+			logicHandler.RunConsoleCommandOnPlayer(pid, "player->removespell sittingAnim_paralyze", false)
+			
 			tableHelper.removeValue(Players[pid].data.spellbook, "sittingAnim_paralyze")				
 
 			logicHandler.RunConsoleCommandOnPlayer(pid, "EnablePlayerViewSwitch", false)
-
+			
 			Players[pid]:LoadSpellbook()
-
+			
 		end
-
+		
 		tes3mp.SetModel(pid, Model)
 
 		tes3mp.SendBaseInfo(pid)	
-
+		
 		tes3mp.PlayAnimation(pid, Animation, 0, 1, true)
 
 		PlayerAnimationList[PlayerName] = {
@@ -174,87 +197,98 @@ AnimationMenu.OnGUIAction = function(eventStatus, pid, idGui, data)
 			model = Model,
 			cellDescription = cellDescription
 		}
-
-	end	
-end
-
-AnimationMenu.ChatListener = function(pid, cmd)
-	
-	if cmd[1] == "anim" and cmd[2] == nil then
-
-		AnimationMenu.showAnimMenu(pid)
-
-	end	
+		
+	end
 end
 
 AnimationMenu.OnPlayerAuthentified = function(eventStatus, pid)
 
 	local PlayerName = GetName(pid)
 
+	local PlayerRace = Players[pid].data.character.race
+	
+	local PlayerGender = Players[pid].data.character.gender
+	
 	local Model = "base_anim.nif"
-
+	
+	if PlayerGender == 0 then
+	
+		Model = "base_anim_female.nif"	
+		
+	end
+	
+	if PlayerRace == "argonian" or PlayerRace == "khajiit" then
+	
+		Model = "base_animkna.nif"
+		
+	end
+	
 	tes3mp.SetModel(pid, Model)
-
+	
 	tes3mp.SendBaseInfo(pid)
 
 	if tableHelper.containsValue(Players[pid].data.spellbook, "sittingAnim_paralyze") then
-
-		logicHandler.RunConsoleCommandOnPlayer(pid, "player->removespell sittingAnim_paralyze")
-
+	
+		logicHandler.RunConsoleCommandOnPlayer(pid, "player->removespell sittingAnim_paralyze", false)
+		
 		tableHelper.removeValue(Players[pid].data.spellbook, "sittingAnim_paralyze")
-
+		
 		Players[pid]:LoadSpellbook()		
-
+		
 		tes3mp.PlayAnimation(pid, "idle", 0, 1, true)
-
+		
 	end
 end
 
 AnimationMenu.OnPlayerCellChange = function(eventStatus, pid, playerPacket, previousCellDescription)
 
 	local PlayerName = GetName(pid)
-
+	
 	for targetName, data in pairs(PlayerAnimationList) do
-
+		
 		if targetName ~= PlayerName and playerPacket.location.cell == data.cellDescription then
-
+			
 			local targetPid = logicHandler.GetPlayerByName(targetName).pid
-
-			if targetPid and Players[targetPid] ~= nil and Players[targetPid]:IsLoggedIn() then
-
+			
+			if targetPid and Players[targetPid] and Players[targetPid]:IsLoggedIn() then
+			
 				tes3mp.SetModel(targetPid, data.model)
 
 				tes3mp.SendBaseInfo(targetPid)
-
-				tes3mp.PlayAnimation(targetPid, data.animation, 0, 1, true)					
-			end				
-		end			
-	end	
+		
+				tes3mp.PlayAnimation(targetPid, data.animation, 0, 1, true)
+				
+			end
+			
+		end
+		
+	end
 end
 
 AnimationMenu.OnPlayerDisconnect = function(eventStatus, pid)
 
 	local PlayerName = GetName(pid)
-
+	
 	if PlayerAnimationList[PlayerName] then
-
+	
 		PlayerAnimationList[PlayerName] = nil
-	end	
+		
+	end
 end
 
 ------------
 -- EVENTS --
 ------------
-customEventHooks.registerHandler("OnServerPostInit", AnimationMenu.OnServerPostInitHandler)
+customEventHooks.registerHandler("OnServerPostInit", AnimationMenu.OnServerPostInit)
 
 customEventHooks.registerHandler("OnPlayerAuthentified", AnimationMenu.OnPlayerAuthentified)
 
 customEventHooks.registerHandler("OnPlayerCellChange", AnimationMenu.OnPlayerCellChange)
 
-customEventHooks.registerHandler("OnPlayerDisconnect", AnimationMenu.OnPlayerDisconnect)
+customEventHooks.registerValidator("OnPlayerDisconnect", AnimationMenu.OnPlayerDisconnect)
 
 customEventHooks.registerHandler("OnGUIAction", AnimationMenu.OnGUIAction)
 
-customCommandHooks.registerCommand("anim", AnimationMenu.ChatListener)
+customCommandHooks.registerCommand("anim", AnimationMenu.showAnimMenu)
 
 return AnimationMenu
