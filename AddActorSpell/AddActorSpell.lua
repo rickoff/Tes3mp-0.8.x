2080f60ce025f312f5e94c968a4dd86a563042a4
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-01-12 12:44:02 +0100

    Update AddActorSpell.lua

--- AddActorSpell/AddActorSpell.lua
+++ AddActorSpell/AddActorSpell.lua
@@ -18,7 +18,7 @@ local spellsTable = {--add/modify spells from list example
 		stackingState = false,--Whether the spell should stack with other instances of itself
 		effects = {
 			{
-				id = 117,--The id of the effect, refer to emuneration.lua
+				id = 117,--The id of the effect, refer to server/scripts/enumerations.lua
 				magnitude = 100,--The magnitude of the effect
 				duration = 999,--The duration of the effect
 				timeLeft = 999,--The timeLeft for the effect
@@ -51,7 +51,7 @@ local spellsTable = {--add/modify spells from list example
 customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid, playerPacket, previousCellDescription)
 	local AddSpell = false
 	local cellDescription = playerPacket.location.cell
-    for _, uniqueIndex in ipairs(LoadedCells[cellDescription].data.packets.actorList) do
+	for _, uniqueIndex in ipairs(LoadedCells[cellDescription].data.packets.actorList) do
 		if LoadedCells[cellDescription].data.objectData[uniqueIndex] then	
 			local objectData = LoadedCells[cellDescription].data.objectData[uniqueIndex]
 			if actorList[string.lower(objectData.refId)] then
@@ -71,7 +71,7 @@ customEventHooks.registerHandler("OnPlayerCellChange", function(eventStatus, pid
 				end
 			end
 		end
-    end
+	end
 	if AddSpell then	
 		local objectsData = LoadedCells[cellDescription].data.objectData
 		local packetSpell = LoadedCells[cellDescription].data.packets.spellsActive
