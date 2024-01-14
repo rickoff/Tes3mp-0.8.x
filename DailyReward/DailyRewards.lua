4a5678db91c1f4908f59a57e3fb74ffaf85e0d37
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-01-14 16:16:55 +0100

    Update DailyRewards.lua

--- DailyReward/DailyRewards.lua
+++ DailyReward/DailyRewards.lua
@@ -7,8 +7,7 @@ gives daily login rewards
 ---------------------------
 INSTALLATION:
 Save the file as DailyRewards.lua inside your server/scripts/custom folder.
-Edits to customScripts.lua
-DailyRewards = require("custom.DailyRewards")
+Edits to customScripts.lua add in : require("custom.DailyRewards")
 ---------------------------
 CONFIGURATION:
 change cfg.randomizeReward to false gives users the entire table, true randomizes
@@ -65,137 +64,74 @@ local dailyRewardsTable = {
 }
 
 local function AddObjectInventoryPlayer(pid, item)	
-
 	local itemref = {
 		refId = item.refId,
 		count = item.count or 1,
 		charge = item.charge or -1,
 		enchantmentCharge = item.enchantmentCharge or -1,
 		soul = item.soul or ""
-	}	
-	
+	}		
 	local indexLoc = inventoryHelper.getItemIndex(Players[pid].data.inventory, itemref.refId, itemref.charge, itemref.enchantmentCharge, itemref.soul)
-
-	if indexLoc then
-	
-		if Players[pid].data.inventory[indexLoc].count then
-			
-			Players[pid].data.inventory[indexLoc].count = Players[pid].data.inventory[indexLoc].count + itemref.count
-			
-		else
-		
-			Players[pid].data.inventory[indexLoc].count = itemref.count
-			
-		end
-		
-	else
-		
-		table.insert(Players[pid].data.inventory, itemref)	
-		
+	if indexLoc then	
+		if Players[pid].data.inventory[indexLoc].count then			
+			Players[pid].data.inventory[indexLoc].count = Players[pid].data.inventory[indexLoc].count + itemref.count			
+		else		
+			Players[pid].data.inventory[indexLoc].count = itemref.count			
+		end		
+	else		
+		table.insert(Players[pid].data.inventory, itemref)			
 	end
-
-	Players[pid]:LoadItemChanges({itemref}, enumerations.inventory.ADD)
-	
-	Players[pid]:QuicksaveToDrive()
-	
+	Players[pid]:LoadItemChanges({itemref}, enumerations.inventory.ADD)	
 end
 
-local function randomPull(pid, targetTable)
-	
+local function randomPull(pid, targetTable)	
 	math.randomseed((pid * (1 + math.random())) * os.time() + math.random())
 	math.random()
 	math.random()
-	math.random()
-	
-	local keyset = {}
-	
-	for k in pairs(targetTable) do
-	
-		table.insert(keyset, k)
-		
-	end
-	
-	return targetTable[keyset[math.random(#keyset)]]
-	
+	math.random()	
+	local keyset = {}	
+	for k in pairs(targetTable) do	
+		table.insert(keyset, k)		
+	end	
+	return targetTable[keyset[math.random(#keyset)]]	
 end
 
 local function giveDailyLoginItem(pid)
-	
-	local addedItems = {}
-	
-	local rewardMsg = trad.rewardLoginMessageDaily
-	
-	if cfg.randomizeReward == true then
-
-		local item = randomPull(pid, dailyRewardsTable)
-		
-		if item then
-		
-			AddObjectInventoryPlayer(pid, item)
-			
-			table.insert(addedItems, item)
-			
-		end
-		
+	local addedItems = {}	
+	local rewardMsg = trad.rewardLoginMessageHeader	
+	if cfg.randomizeDailyReward == true then
+		local item = randomPull(pid, dailyRewardsTable)		
+		if item then		
+			AddObjectInventoryPlayer(pid, item)			
+			table.insert(addedItems, item)			
+		end		
 	else
-
-		for i=1,#dailyRewardsTable do
-		
-			local item = dailyRewardsTable[i]
-			
-			if item then
-			
-				AddObjectInventoryPlayer(pid, item)
-				
-				table.insert(addedItems, item)
-				
-			end
-			
-		end
-		
-	end
-	
-	for i = 1, #addedItems do
-	
-		local item = addedItems[i]
-		
-		local iCount = item.count
-		
+		for i=1,#dailyRewardsTable do		
+			local item = dailyRewardsTable[i]			
+			if item then			
+				AddObjectInventoryPlayer(pid, item)				
+				table.insert(addedItems, item)				
+			end			
+		end		
+	end	
+	for i = 1, #addedItems do	
+		local item = addedItems[i]		
+		local iCount = item.count		
 		rewardMsg = rewardMsg.."\n"..color.White..item.name.." ("..item.count..")"
-
-	end
-	
-	logicHandler.RunConsoleCommandOnPlayer(pid, "PlaySoundVP \""..sound.name.."\" "..sound.volume.." "..sound.pitch, false)
-	
+	end	
+	logicHandler.RunConsoleCommandOnPlayer(pid, "PlaySoundVP \""..sound.name.."\" "..sound.volume.." "..sound.pitch, false)	
 	tes3mp.MessageBox(pid, -1, rewardMsg)
-
 end
 
-local DailyRewards = {}
-
-DailyRewards.OnPlayerAuthentified = function(eventStatus, pid)
-	
-	local daysName = os.date("%A")
-
-	if not Players[pid].data.customVariables.DailyRewards then
-
+customEventHooks.registerHandler("OnPlayerAuthentified", function(eventStatus, pid)	
+	local daysName = os.date("%A")	
+	if not Players[pid].data.customVariables.DailyRewards then	
 		Players[pid].data.customVariables.DailyRewards = {
 			days = ""
-		}
-
-	end
-
-	if Players[pid].data.customVariables.DailyRewards.days ~= daysName then
-
-		giveDailyLoginItem(pid)
-
-		Players[pid].data.customVariables.DailyRewards.days = daysName			
-
-		Players[pid]:QuicksaveToDrive()
-
+		}		
 	end	
-end
-
-customEventHooks.registerHandler("OnPlayerAuthentified", DailyRewards.OnPlayerAuthentified)
-
-return DailyRewards
+	if Players[pid].data.customVariables.DailyRewards.days ~= daysName then	
+		giveDailyLoginItem(pid)		
+		Players[pid].data.customVariables.DailyRewards.days = daysName		
+	end
+end)
