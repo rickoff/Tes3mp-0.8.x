bf74b01899d0297c835ad6fd1d17a948c420f70e
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-01-14 14:59:21 +0100

    Update MultiMark.lua

--- MultiMark/MultiMark.lua
+++ MultiMark/MultiMark.lua
@@ -1,17 +1,14 @@
 --[[
+--[[
 MultiMark
 tes3mp 0.8.1
 script version 1.0
 ---------------------------
-DESCRIPTION :
-USE DIRECTLY SPELL MARK AND RECALL
----------------------------
 INSTALLATION:
 Save the file as MultiMark.lua inside your server/scripts/custom folder.
-Edits to customScripts.lua add :
-MultiMark = require("custom.MultiMark")
+Edits to customScripts.lua add : require("custom.MultiMark")
 ---------------------------
-cfg :
+CONFIGURATION :
 Change trad in your language for your server
 Change cfg MaxMark for config limite mark
 Change cfg GUI numbers for a unique numbers 
@@ -29,6 +26,7 @@ local trad = {
 }
 
 local cfg = {
+	OnServerInit = false,	
 	MaxMark = 5,
 	costMark = 18,
 	costRecall = 18,
@@ -48,14 +46,14 @@ local function SelectChoice(pid, index)
 end
 
 local function ListMark(pid)
-	local options = Players[pid].data.customVariables.markLocation
-	local list = trad.BackList	
-	for i = 1, #options do
+    local options = Players[pid].data.customVariables.markLocation
+    local list = trad.BackList	
+    for i = 1, #options do
 		list = list..string.sub(options[i].cell, 1, 25).." : "..math.floor(options[i].posX).." ; "..math.floor(options[i].posY).." ; "..math.floor(options[i].posZ)		
-		if not(i == #options) then
-			list = list .. "\n"
-		end
-	end
+        if not(i == #options) then
+            list = list .. "\n"
+        end
+    end
 	tes3mp.ListBox(pid, cfg.MainGUI, trad.SelectMark..color.Default, list)	
 end
 
@@ -78,7 +76,6 @@ local function AddMark(pid)
 		tablePos.rotZ = tes3mp.GetRotZ(pid) 			
 		table.insert(Players[pid].data.customVariables.markLocation, tablePos)
 		tes3mp.MessageBox(pid, -1, trad.AddNewMark..color.Green..tablePos.cell.."\n"..color.White..math.floor(tablePos.posX).." ; "..math.floor(tablePos.posY).." ; "..math.floor(tablePos.posZ))
-		Players[pid]:QuicksaveToDrive()
 	else
 		tes3mp.MessageBox(pid, -1, trad.LimiteMark..color.Green..cfg.MaxMark)
 	end
@@ -88,7 +85,6 @@ local function RemoveMark(pid)
 	local index = playerIndex[GetName(pid)]
 	Players[pid].data.customVariables.markLocation[index] = nil	
 	tableHelper.cleanNils(Players[pid].data.customVariables.markLocation)
-	Players[pid]:QuicksaveToDrive()
 end
 
 local function RecallPlayer(pid)
@@ -106,133 +102,133 @@ local function RecallPlayer(pid)
 	tes3mp.SendPos(pid)		
 end
 
-local MultiMark = {}
+customEventHooks.registerHandler("OnServerInit", function(eventStatus)
+	if cfg.OnServerInit then
+		local recordStoreSpells = RecordStores["spell"]
+		local recordTable
+		recordTable = {
+			name = trad.NameMark,
+			cost = cfg.costMark,	  
+			subtype = 0,
+			flags = 1,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 60,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreSpells.data.permanentRecords["mark"] = recordTable
+		recordTable = {
+			name = trad.NameRecall,
+			cost = cfg.costRecall,		
+			subtype = 0,
+			flags = 1,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 61,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreSpells.data.permanentRecords["recall"] = recordTable
+		recordStoreSpells:Save()
+		local recordStoreEnchant = RecordStores["enchantment"]	
+		recordTable = {
+			cost = cfg.costMark,		
+			subtype = 2,
+			flags = 90,
+			charge = 90,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 60,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreEnchant.data.permanentRecords["mark_en"] = recordTable	
+		recordStoreEnchant.data.permanentRecords["markring_en"] = recordTable		
+		recordTable = {
+			cost = cfg.costMark,		
+			subtype = 0,
+			flags = 18,
+			charge = 18,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 60,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreEnchant.data.permanentRecords["sc_mark_en"] = recordTable
+		recordTable = {
+			cost = cfg.costRecall,		
+			subtype = 2,
+			flags = 90,
+			charge = 90,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 61,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreEnchant.data.permanentRecords["recallring_en"] = recordTable	
+		recordStoreEnchant.data.permanentRecords["recall_en"] = recordTable		
+		recordTable = {
+			cost = cfg.costRecall,		
+			subtype = 0,
+			flags = 18,
+			charge = 18,
+			effects = {
+				{
+					attribute = -1,
+					area = 0,
+					duration = 0,
+					id = 61,
+					rangeType = 0,
+					skill = -1,
+					magnitudeMax = 0,
+					magnitudeMin = 0
+				}
+			}
+		}
+		recordStoreEnchant.data.permanentRecords["sc_leaguestep_en"] = recordTable	
+		recordStoreEnchant:Save()
+	end
+end)
 
-MultiMark.OnServerInit = function(eventStatus)
-	local recordStoreSpells = RecordStores["spell"]
-	local recordTable
-	recordTable = {
-		name = trad.NameMark,
-		cost = cfg.costMark,	  
-		subtype = 0,
-		flags = 1,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 60,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreSpells.data.permanentRecords["mark"] = recordTable
-	recordTable = {
-		name = trad.NameRecall,
-		cost = cfg.costRecall,		
-		subtype = 0,
-		flags = 1,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 61,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreSpells.data.permanentRecords["recall"] = recordTable
-	recordStoreSpells:Save()
-	local recordStoreEnchant = RecordStores["enchantment"]	
-	recordTable = {
-		cost = cfg.costMark,		
-		subtype = 2,
-		flags = 90,
-		charge = 90,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 60,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreEnchant.data.permanentRecords["mark_en"] = recordTable	
-	recordStoreEnchant.data.permanentRecords["markring_en"] = recordTable		
-	recordTable = {
-		cost = cfg.costMark,		
-		subtype = 0,
-		flags = 18,
-		charge = 18,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 60,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreEnchant.data.permanentRecords["sc_mark_en"] = recordTable
-	recordTable = {
-		cost = cfg.costRecall,		
-		subtype = 2,
-		flags = 90,
-		charge = 90,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 61,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreEnchant.data.permanentRecords["recallring_en"] = recordTable	
-	recordStoreEnchant.data.permanentRecords["recall_en"] = recordTable		
-	recordTable = {
-		cost = cfg.costRecall,		
-		subtype = 0,
-		flags = 18,
-		charge = 18,
-		effects = {
-			{
-				attribute = -1,
-				area = 0,
-				duration = 0,
-				id = 61,
-				rangeType = 0,
-				skill = -1,
-				magnitudeMax = 0,
-				magnitudeMin = 0
-			}
-		}
-	}
-	recordStoreEnchant.data.permanentRecords["sc_leaguestep_en"] = recordTable	
-	recordStoreEnchant:Save()
-end
-
-MultiMark.OnPlayerSpellsActiveHandler = function(eventStatus, pid, playerPacket)
+customEventHooks.registerHandler("OnPlayerSpellsActive", function(eventStatus, pid, playerPacket)
 	for spellId, spellInstances in pairs(playerPacket.spellsActive) do	
 		for _, spellInstance in ipairs(spellInstances) do	
 			for _, effect in ipairs(spellInstance.effects) do		
@@ -246,9 +242,9 @@ MultiMark.OnPlayerSpellsActiveHandler = function(eventStatus, pid, playerPacket)
 			end	
 		end
 	end
-end
+end)
 
-MultiMark.OnGUIAction = function(eventStatus, pid, idGui, data)
+customEventHooks.registerHandler("OnGUIAction", function(eventStatus, pid, idGui, data)
 	if idGui == cfg.MainGUI then
 		if tonumber(data) == 0 or tonumber(data) == 18446744073709551615 then
 		else   
@@ -262,35 +258,23 @@ MultiMark.OnGUIAction = function(eventStatus, pid, idGui, data)
 			ListMark(pid)		
 		end
 	end
-end
+end)
 
-MultiMark.OnPlayerAuthentified = function(eventStatus, pid)
+customEventHooks.registerHandler("OnPlayerAuthentified", function(eventStatus, pid)
 	if not Players[pid].data.customVariables.markLocation then
 		Players[pid].data.customVariables.markLocation = {}
-		Players[pid]:QuicksaveToDrive()
 	end
-end
+end)
 
-MultiMark.OnRecordDynamicValidator = function(eventStatus, pid, recordArray, storeType)
+customEventHooks.registerValidator("OnRecordDynamic", function(eventStatus, pid, recordArray, storeType)
 	if storeType == "enchantment" or storeType == "spell" or storeType == "potion" then	
 		for _, record in ipairs(recordArray) do			
 			for _, effect in ipairs(record.effects) do			
-				if effect.id == enumerations.effects.MARK then
+				if effect.id == enumerations.effects.MARK or effect.id == enumerations.effects.RECALL then
 					effect.magnitudeMin = 0					
-					effect.magnitudeMax = 0			
-				elseif effect.id == enumerations.effects.RECALL then			
-					effect.magnitudeMin = 0					
-					effect.magnitudeMax = 0					
+					effect.magnitudeMax = 0				
 				end					
 			end	
 		end
 	end
-end	
-
-customEventHooks.registerValidator("OnRecordDynamic", MultiMark.OnRecordDynamicValidator)
-customEventHooks.registerHandler("OnGUIAction", MultiMark.OnGUIAction)
-customEventHooks.registerHandler("OnServerInit", MultiMark.OnServerInit)
-customEventHooks.registerHandler("OnPlayerAuthentified", MultiMark.OnPlayerAuthentified)
-customEventHooks.registerHandler("OnPlayerSpellsActive", MultiMark.OnPlayerSpellsActiveHandler)
-
-return MultiMark
+end)
