023beaf3a2b32dfba4fa9794c8282d2e41727bd7
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-01-14 15:45:56 +0100

    Update MovePlease.lua

--- MovePlease/MovePlease.lua
+++ MovePlease/MovePlease.lua
@@ -1,44 +1,28 @@
 --[[
-MovePlease by Rickoff
+MovePlease
 tes3mp 0.8.1
 ---------------------------
 DESCRIPTION :
-I was tired of being stuck in hallways.
 activating an actor with the hands to cast a spell makes him move.
 ---------------------------
 INSTALLATION:
 Save the file as MovePlease.lua inside your server/scripts/custom folder.
-Edits to customScripts.lua
-MovePlease = require("custom.MovePlease")
+Edits to customScripts.lua add in : require("custom.MovePlease")
 ---------------------------
 ]]
 local cfg = {
 	OnlyInterior = true
 }
 
-local MovePlease = {}
-
-MovePlease.OnActivatedObject = function(eventStatus, pid, cellDescription, objects)
-	
+customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid, cellDescription, objects)
 	if tes3mp.GetDrawState(pid) ~= 2 then return end
-
-	if cfg.OnlyInterior then
-		if tes3mp.IsInExterior(pid) then return end
-	end
-	
-	local ObjectIndex
-
+	if cfg.OnlyInterior and tes3mp.IsInExterior(pid) then return end
 	for _, object in pairs(objects) do
-		ObjectIndex = object.uniqueIndex
+		if object.uniqueIndex and tableHelper.containsValue(LoadedCells[cellDescription].data.packets.actorList, object.uniqueIndex)
+		and not tableHelper.containsValue(LoadedCells[cellDescription].data.packets.death, object.uniqueIndex) then	
+			logicHandler.RunConsoleCommandOnObject(pid, "PlayGroup, hit2, 1", cellDescription, object.uniqueIndex, false)	
+			logicHandler.RunConsoleCommandOnObject(pid, "AIWander, 512, 1, 0", cellDescription, object.uniqueIndex, false)		
+			return customEventHooks.makeEventStatus(false,false) 	
+		end
 	end
-
-	if ObjectIndex and tableHelper.containsValue(LoadedCells[cellDescription].data.packets.actorList, ObjectIndex) then
-		logicHandler.RunConsoleCommandOnObject(pid, "PlayGroup, hit2, 1", cellDescription, ObjectIndex, false)	
-		logicHandler.RunConsoleCommandOnObject(pid, "AIWander, 512, 1, 0", cellDescription, ObjectIndex, false)
-		return customEventHooks.makeEventStatus(false,false) 		
-	end
-end
-
-customEventHooks.registerValidator("OnObjectActivate", MovePlease.OnActivatedObject)
-
-return MovePlease
+end)
