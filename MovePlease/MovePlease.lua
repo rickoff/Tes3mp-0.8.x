68c0a276c8072d9abc95617564a6bd88df762dce
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-02-02 11:58:53 +0100

    Update MovePlease.lua
    add blacklist config

--- MovePlease/MovePlease.lua
+++ MovePlease/MovePlease.lua
@@ -14,12 +14,30 @@ local cfg = {
 	OnlyInterior = true
 }
 
+local blackListCellDescription = {
+	"" = true,
+	"" = true
+}
+
+local blackListRefId = {
+	"" = true,
+	"" = true
+}
+
+local blackListUniqueIndex = {
+	"" = true,
+	"" = true
+}
+
 customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid, cellDescription, objects)
 	if tes3mp.GetDrawState(pid) ~= 2 then return end
 	if cfg.OnlyInterior and tes3mp.IsInExterior(pid) then return end
+	if blackListCellDescription then return end
 	for _, object in pairs(objects) do
-		if object.uniqueIndex and tableHelper.containsValue(LoadedCells[cellDescription].data.packets.actorList, object.uniqueIndex)
-		and not tableHelper.containsValue(LoadedCells[cellDescription].data.packets.death, object.uniqueIndex) then	
+		if object.uniqueIndex and object.refId and tableHelper.containsValue(LoadedCells[cellDescription].data.packets.actorList, object.uniqueIndex)
+		and not tableHelper.containsValue(LoadedCells[cellDescription].data.packets.death, object.uniqueIndex) then
+			if blackListRefId[object.refId] then return end
+			if blackListUniqueIndex[object.uniqueIndex] then return end			
 			logicHandler.RunConsoleCommandOnObject(pid, "PlayGroup, hit2, 1", cellDescription, object.uniqueIndex, false)	
 			logicHandler.RunConsoleCommandOnObject(pid, "AIWander, 512, 1, 0", cellDescription, object.uniqueIndex, false)		
 			return customEventHooks.makeEventStatus(false,false) 	
