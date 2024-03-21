f5d60825995d6cd1f3a825a5202e2b43a60c2697
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-03-22 00:38:11 +0100

    Update FixBarterDuplicate.lua

--- FixBarterDuplicate/FixBarterDuplicate.lua
+++ FixBarterDuplicate/FixBarterDuplicate.lua
@@ -11,6 +11,11 @@ require("custom.FixBarterDuplicate")
 
 local tempBarter = {}
 local tempPlayers = {}
+local tempActivate = {}
+
+function OnObjectActivateTimer(uniqueIndex)
+    tempActivate[uniqueIndex] = nil
+end
 
 local function GetName(pid)
 	if Players[pid] then
@@ -41,16 +46,35 @@ local function CheckInventoryActorItem(pid, uniqueIndex, inventory)
 	return rejected, rejectedItems
 end
 
+customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid, cellDescription, objects) 
+	for _, object in pairs(objects) do
+		if object.uniqueIndex then   
+			if tempActivate[object.uniqueIndex] then
+				return customEventHooks.makeEventStatus(false, false)
+			else
+				tempActivate[object.uniqueIndex] = true
+				local timerActivate = tes3mp.CreateTimerEx("OnObjectActivateTimer", time.seconds(1), "s", object.uniqueIndex)
+				tes3mp.StartTimer(timerActivate)
+			end
+		end
+    end
+end)
+
 customEventHooks.registerHandler("OnObjectActivate", function(eventStatus, pid, cellDescription, objects)	
-	if tempPlayers[GetName(pid)] then
-		tempPlayers[GetName(pid)] = nil
+	for _, object in pairs(objects) do
+		if object.activatingPid and tempPlayers[GetName(object.activatingPid)] then
+			tempPlayers[GetName(object.activatingPid)] = nil
+		end
 	end
 end)
 
 customEventHooks.registerHandler("OnObjectDialogueChoice", function(eventStatus, pid, cellDescription, objects)
 	for _, object in pairs(objects) do
 		if object.refId and object.uniqueIndex and object.dialogueChoiceType == enumerations.dialogueChoice.BARTER then
-			tempPlayers[GetName(pid)] = object.uniqueIndex
+			tempPlayers[GetName(pid)] = {
+				uniqueIndex = object.uniqueIndex,
+				inventory = {}
+			}
 			tempBarter[object.uniqueIndex] = tableHelper.deepCopy(LoadedCells[cellDescription].data.objectData[object.uniqueIndex].inventory)
 		end
 	end	
@@ -58,11 +82,43 @@ end)
 
 customEventHooks.registerValidator("OnPlayerInventory", function(eventStatus, pid, playerPacket)
 	local playerName = GetName(pid)	
-	if playerPacket.action == enumerations.inventory.ADD and tempPlayers[playerName] then
-		local rejected, rejectedItems = CheckInventoryActorItem(pid, tempPlayers[playerName], playerPacket.inventory)
+	if playerPacket.action == enumerations.inventory.ADD and tempPlayers[playerName] then	
+		local rejected, rejectedItems = CheckInventoryActorItem(pid, tempPlayers[playerName].uniqueIndex, playerPacket.inventory)
 		if rejected then
+			tempPlayers[playerName].inventory = rejectedItems				
 			Players[pid]:LoadItemChanges(rejectedItems, enumerations.inventory.REMOVE)
 			return customEventHooks.makeEventStatus(false, false)			
+		end
+	end
+end)
+
+customEventHooks.registerValidator("OnContainer", function(eventStatus, pid, cellDescription, objects)	
+	for _, object in pairs(objects) do
+		if object.uniqueIndex and tempBarter[object.uniqueIndex] then		
+			local action = tes3mp.GetObjectListAction()				
+			if action == enumerations.container.REMOVE then			
+				for containerIndex = 0, tes3mp.GetObjectListSize() - 1 do
+					for itemIndex = 0, tes3mp.GetContainerChangesSize(containerIndex) - 1 do
+						local item = {
+							refId = tes3mp.GetContainerItemRefId(containerIndex, itemIndex),
+							count = tes3mp.GetContainerItemCount(containerIndex, itemIndex),
+							charge = tes3mp.GetContainerItemCharge(containerIndex, itemIndex),
+							enchantmentCharge = tes3mp.GetContainerItemEnchantmentCharge(containerIndex, itemIndex),
+							soul = tes3mp.GetContainerItemSoul(containerIndex, itemIndex)
+						}
+						for _, inv in ipairs(tempPlayers[GetName(pid)].inventory) do
+							if inv.refId == item.refId
+							and inv.count == item.count
+							and inv.charge == item.charge
+							and inv.enchantmentCharge == item.enchantmentCharge
+							and inv.soul == item.soul then
+								inventoryHelper.addItem(Players[pid].data.inventory, item.refId, item.count, item.charge, item.enchantmentCharge, item.soul)
+								Players[pid]:LoadItemChanges({item}, enumerations.inventory.ADD)								
+							end
+						end
+					end
+				end
+			end
 		end
 	end
 end)
