23e71f429ee80d5c0c081d1b264f113bace05df9
Rickoff <38435435+rickoff@users.noreply.github.com>
2024-03-29 01:20:00 +0100

    Update FixBarterDuplicate.lua

--- FixBarterDuplicate/FixBarterDuplicate.lua
+++ FixBarterDuplicate/FixBarterDuplicate.lua
@@ -9,7 +9,7 @@ require("custom.FixBarterDuplicate")
 ---------------------------
 ]]
 
-local tempBarter = {}
+local tempBarters = {}
 local tempPlayers = {}
 local tempActivate = {}
 
@@ -21,29 +21,6 @@ local function GetName(pid)
 	if Players[pid] then
 		return string.lower(Players[pid].accountName)
 	end
-end
-
-local function CheckInventoryActorItem(pid, uniqueIndex, inventory)
-	local rejected = false
-	local rejectedItems = {}
-	if tempBarter[uniqueIndex] then
-		local inventoryActor = tempBarter[uniqueIndex]
-		for _, item in pairs(inventory) do
-			if item.refId ~= "gold_001" then
-				local itemIndex = inventoryHelper.getItemIndex(inventoryActor, item.refId, item.charge, item.enchantmentCharge, item.soul)
-				if inventoryActor[itemIndex] and inventoryActor[itemIndex].count >= item.count then
-					inventoryActor[itemIndex].count = inventoryActor[itemIndex].count - item.count
-					if inventoryActor[itemIndex].count == 0 then
-						inventoryActor[itemIndex] = nil
-					end
-				else
-					rejected = true
-					table.insert(rejectedItems, item)
-				end
-			end
-		end
-	end	
-	return rejected, rejectedItems
 end
 
 customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid, cellDescription, objects) 
@@ -60,41 +37,47 @@ customEventHooks.registerValidator("OnObjectActivate", function(eventStatus, pid
     end
 end)
 
-customEventHooks.registerHandler("OnObjectActivate", function(eventStatus, pid, cellDescription, objects)	
+customEventHooks.registerHandler("OnObjectActivate", function(eventStatus, pid, cellDescription, objects)
 	for _, object in pairs(objects) do
-		if object.activatingPid and tempPlayers[GetName(object.activatingPid)] then
+		if object.activatingPid and object.uniqueIndex and tempPlayers[GetName(object.activatingPid)] then
+			tempBarters[tempPlayers[GetName(object.activatingPid)].uniqueIndex] = nil
 			tempPlayers[GetName(object.activatingPid)] = nil
 		end
 	end
 end)
 
 customEventHooks.registerHandler("OnObjectDialogueChoice", function(eventStatus, pid, cellDescription, objects)
+	local playerName = GetName(pid)
 	for _, object in pairs(objects) do
 		if object.refId and object.uniqueIndex and object.dialogueChoiceType == enumerations.dialogueChoice.BARTER then
-			tempPlayers[GetName(pid)] = {
+			tempPlayers[playerName] = {
 				uniqueIndex = object.uniqueIndex,
 				inventory = {}
 			}
-			tempBarter[object.uniqueIndex] = tableHelper.deepCopy(LoadedCells[cellDescription].data.objectData[object.uniqueIndex].inventory)
+			tempBarters[object.uniqueIndex] = true
 		end
 	end	
 end)
 
 customEventHooks.registerValidator("OnPlayerInventory", function(eventStatus, pid, playerPacket)
 	local playerName = GetName(pid)	
-	if playerPacket.action == enumerations.inventory.ADD and tempPlayers[playerName] then	
-		local rejected, rejectedItems = CheckInventoryActorItem(pid, tempPlayers[playerName].uniqueIndex, playerPacket.inventory)
-		if rejected then
-			tempPlayers[playerName].inventory = rejectedItems				
-			Players[pid]:LoadItemChanges(rejectedItems, enumerations.inventory.REMOVE)
-			return customEventHooks.makeEventStatus(false, false)			
+	local rejectedItem = {}
+	if playerPacket.action == enumerations.inventory.ADD and tempPlayers[playerName] then
+		for _, item in pairs(playerPacket.inventory) do
+			if item.refId ~= "gold_001" then 
+				table.insert(rejectedItem, item)
+			end
 		end
+		tempPlayers[playerName].inventory = rejectedItem				
+		Players[pid]:LoadItemChanges(rejectedItem, enumerations.inventory.REMOVE)
+		return customEventHooks.makeEventStatus(false, false)			
 	end
 end)
 
-customEventHooks.registerValidator("OnContainer", function(eventStatus, pid, cellDescription, objects)	
+customEventHooks.registerValidator("OnContainer", function(eventStatus, pid, cellDescription, objects)
+	local playerName = GetName(pid)	
 	for _, object in pairs(objects) do
-		if object.uniqueIndex and tempBarter[object.uniqueIndex] then		
+		if tempPlayers[playerName] then			
 			local action = tes3mp.GetObjectListAction()				
 			if action == enumerations.container.REMOVE then			
 				for containerIndex = 0, tes3mp.GetObjectListSize() - 1 do
@@ -106,13 +89,13 @@ customEventHooks.registerValidator("OnContainer", function(eventStatus, pid, cel
 							enchantmentCharge = tes3mp.GetContainerItemEnchantmentCharge(containerIndex, itemIndex),
 							soul = tes3mp.GetContainerItemSoul(containerIndex, itemIndex)
 						}
-						for _, inv in ipairs(tempPlayers[GetName(pid)].inventory) do
+						for _, inv in pairs(tempPlayers[playerName].inventory) do
 							if inv.refId == item.refId
-							and inv.count == item.count
+							and inv.count <= item.count
 							and inv.charge == item.charge
 							and inv.enchantmentCharge == item.enchantmentCharge
-							and inv.soul == item.soul then
-								inventoryHelper.addItem(Players[pid].data.inventory, item.refId, item.count, item.charge, item.enchantmentCharge, item.soul)
+							and inv.soul == item.soul then					
+								inventoryHelper.addItem(Players[pid].data.inventory, inv.refId, inv.count, inv.charge, inv.enchantmentCharge, inv.soul)
 								Players[pid]:LoadItemChanges({inv}, enumerations.inventory.ADD)								
 							end
 						end
